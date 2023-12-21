b0f02a61a71fca85946ebaab926be7e125641a88
Łukasz Wałowski <lwalowski2@gmail.com>
2023-12-21 09:25:51 +0100

    Zadanie domowe z dnia 13

--- ChallengeApp/Employee.cs
+++ ChallengeApp/Employee.cs
@@ -32,24 +32,24 @@
 			{
 				case 'A':
 				case 'a':
-					this.grades.Add(100);
-					break;
+				   this.grades.Add(100);
+				   break;
 				case 'B':
 				case 'b':
-					this.grades.Add(80);
-					break;
+				   this.grades.Add(80);
+				   break;
 				case 'C':
 				case 'c':
-					this.grades.Add(60);
-					break;
+				   this.grades.Add(60);
+				   break;
 				case 'D':
 				case 'd':
-					this.grades.Add(40);
-					break;
+				   this.grades.Add(40);
+				   break;
 				case 'E':
 				case 'e':
-					this.grades.Add(20);
-					break;
+				   this.grades.Add(20);
+				   break;
 				default:
 					throw new Exception("Wrong letter");
 			}
